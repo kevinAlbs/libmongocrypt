83d49a428307f77a042d02d30121607bc2655913
Kevin Albertson <kevin.albertson@mongodb.com>
2023-11-02 08:43:33 -0400

    implement basic `test_configuring_named_kms_providers`

--- test/test-named-kms-providers.c
+++ test/test-named-kms-providers.c
@@ -16,8 +16,26 @@
 
 #include "test-mongocrypt.h"
 
+#define LOCAL_KEK_BASE64                                                                                               \
+    "+ol0TFyLuVvKFSqGzOFGuaOGQnnyfAqalhOv3II/VSxQTCORCGhOmw/IxhthGx0r"                                                 \
+    "2R/NpMWc91qQ8Ieho4QuE9ucToTnpJ4OquFpdZv2IcO4gey3ecZGCl9jPDig8F+a"
+
+#define MYLOCAL_KEK_BASE64                                                                                             \
+    "yPSpsO8FoVkmt+qdTDnw/pJaKriwfI6NLD1yse3BZLd3ZcXb3rAVJEA+/yu/vPzE"                                                 \
+    "8ju7OYTV63AwfLor8Hg9qzo8lyYC6H3RSfdJ9g9aXdCRfGZJgpbpchJUjR06JMLR"
+
 static void test_configuring_named_kms_providers(_mongocrypt_tester_t *tester) {
-    TEST_ERROR("Not yet implemented");
+    // Test that a named local KMS provider can be set.
+    {
+        mongocrypt_t *crypt = mongocrypt_new();
+        mongocrypt_binary_t *kms_providers =
+            TEST_BSON("{'local': {'key': '%s', 'named' : {'mylocal' : {'provider' : 'local', 'key' : '%s'}}}}",
+                      LOCAL_KEK_BASE64,
+                      MYLOCAL_KEK_BASE64);
+        bool ok = mongocrypt_setopt_kms_providers(crypt, kms_providers);
+        ASSERT_OK(ok, crypt);
+        mongocrypt_destroy(crypt);
+    }
 }
 
 void _mongocrypt_tester_install_named_kms_providers(_mongocrypt_tester_t *tester) {
