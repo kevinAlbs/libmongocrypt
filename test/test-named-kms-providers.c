b3ab348deeae4ca4f5dee523d3b7ae2125772ca5
Kevin Albertson <kevin.albertson@mongodb.com>
2023-11-07 08:48:55 -0500

    update failing test with prefix syntax

--- test/test-named-kms-providers.c
+++ test/test-named-kms-providers.c
@@ -24,14 +24,15 @@
     "yPSpsO8FoVkmt+qdTDnw/pJaKriwfI6NLD1yse3BZLd3ZcXb3rAVJEA+/yu/vPzE"                                                 \
     "8ju7OYTV63AwfLor8Hg9qzo8lyYC6H3RSfdJ9g9aXdCRfGZJgpbpchJUjR06JMLR"
 
+#define BSON_STR(...) #__VA_ARGS__
+
 static void test_configuring_named_kms_providers(_mongocrypt_tester_t *tester) {
     // Test that a named local KMS provider can be set.
     {
         mongocrypt_t *crypt = mongocrypt_new();
-        mongocrypt_binary_t *kms_providers =
-            TEST_BSON("{'local': {'key': '%s', 'named' : {'mylocal' : {'provider' : 'local', 'key' : '%s'}}}}",
-                      LOCAL_KEK_BASE64,
-                      MYLOCAL_KEK_BASE64);
+        mongocrypt_binary_t *kms_providers = TEST_BSON(BSON_STR({"local" : {"key" : "%s"}, "local:2" : {"key" : "%s"}}),
+                                                       LOCAL_KEK_BASE64,
+                                                       MYLOCAL_KEK_BASE64);
         bool ok = mongocrypt_setopt_kms_providers(crypt, kms_providers);
         ASSERT_OK(ok, crypt);
         mongocrypt_destroy(crypt);
