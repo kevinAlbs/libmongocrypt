e3ceef5dd98cee2d3aa92d6842a046eaa5ba35d2
Kevin Albertson <kevin.albertson@mongodb.com>
2024-07-19 08:42:16 -0400

    include crypto params in FRPv2 payload

--- src/mc-fle2-find-range-payload-v2.c
+++ src/mc-fle2-find-range-payload-v2.c
@@ -131,6 +131,38 @@ bool mc_FLE2FindRangePayloadV2_serialize(const mc_FLE2FindRangePayloadV2_t *payl
         return false;
     }
 
+    if (use_range_v2) {
+        // Encode parameters that were used to generate the payload.
+        BSON_ASSERT(payload->sparsity.set);
+        if (!BSON_APPEND_INT64(out, "sp", payload->sparsity.value)) {
+            return false;
+        }
+
+        // Precision may be unset.
+        if (payload->precision.set) {
+            BSON_ASSERT(bson_in_range_int32_t_unsigned(payload->precision.value));
+            if (!BSON_APPEND_INT32(out, "pn", (int32_t)payload->precision.value)) {
+                return false;
+            }
+        }
+
+        BSON_ASSERT(payload->trimFactor.set);
+        BSON_ASSERT(bson_in_range_int32_t_unsigned(payload->trimFactor.value));
+        if (!BSON_APPEND_INT32(out, "tf", (int32_t)payload->trimFactor.value)) {
+            return false;
+        }
+
+        BSON_ASSERT(payload->indexMin.value_type != BSON_TYPE_EOD);
+        if (!BSON_APPEND_VALUE(out, "mn", &payload->indexMin)) {
+            return false;
+        }
+
+        BSON_ASSERT(payload->indexMax.value_type != BSON_TYPE_EOD);
+        if (!BSON_APPEND_VALUE(out, "mx", &payload->indexMax)) {
+            return false;
+        }
+    }
+
     return true;
 }
 
