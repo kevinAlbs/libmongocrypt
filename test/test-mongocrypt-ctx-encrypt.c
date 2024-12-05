5ddd733abc34be45b6f08d7bbffecaa4d122f90e
Kevin Albertson <kevin.albertson@mongodb.com>
2024-12-05 11:10:02 -0500

    Test $lookup with mongocryptd and feeding the same schema twice.

--- test/test-mongocrypt-ctx-encrypt.c
+++ test/test-mongocrypt-ctx-encrypt.c
@@ -5969,7 +5969,26 @@ static void _test_lookup(_mongocrypt_tester_t *tester) {
         mongocrypt_destroy(crypt);
     }
 #undef TF
-    // TODO: Test $lookup from a collection with two $jsonSchema configured.
+    // Test $lookup with feeding the same schema twice.
+#define TF(suffix) TEST_FILE("./test/data/lookup/csfle-mongocryptd/" suffix)
+    {
+        mongocrypt_t *crypt = _mongocrypt_tester_mongocrypt(TESTER_MONGOCRYPT_DEFAULT);
+        mongocrypt_ctx_t *ctx = mongocrypt_ctx_new(crypt);
+
+        ASSERT_OK(mongocrypt_ctx_encrypt_init(ctx, "db", -1, TF("01-cmd.json")), ctx);
+
+        ASSERT_STATE_EQUAL(mongocrypt_ctx_state(ctx), MONGOCRYPT_CTX_NEED_MONGO_COLLINFO);
+        {
+            // Feed schema for "c2" twice.
+            ASSERT_OK(mongocrypt_ctx_mongo_feed(ctx, TF("02-collInfo-c1.json")), ctx);
+            ASSERT_OK(mongocrypt_ctx_mongo_feed(ctx, TF("02-collInfo-c2.json")), ctx);
+            ASSERT_FAILS(mongocrypt_ctx_mongo_feed(ctx, TF("02-collInfo-c2.json")), ctx, "got duplicate schemas");
+        }
+
+        mongocrypt_ctx_destroy(ctx);
+        mongocrypt_destroy(crypt);
+    }
+#undef TF
     // TODO: Test $lookup from a collection $jsonSchema and a sibling validator configured.
     // TODO: Test $lookup with mongocryptd and feeding the same schema twice.
     // TODO: Test $lookup with mongocryptd and feeding a non-matching schema.
