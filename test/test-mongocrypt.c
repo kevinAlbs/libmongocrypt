b89a09855c58647000389e93b991469825c61f6a
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-14 08:41:46 -0500

    TEMP: support wildcard

--- test/test-mongocrypt.c
+++ test/test-mongocrypt.c
@@ -967,8 +967,11 @@ int main(int argc, char **argv) {
     _mongocrypt_tester_t tester = {0};
     int i;
 
-    printf("Pass a list of test names to run only specific tests. E.g.:\n");
-    printf("test-mongocrypt _mongocrypt_test_mcgrew\n\n");
+    printf("Run a subset of tests by passing a list of patterns. Patterns may be exact matches or include a trailing "
+           "wildcard (*) Examples:\n");
+    printf("$ test-mongocrypt _mongocrypt_test_mcgrew\n");
+    printf("$ test-mongocrypt test_mc_schema_broker*\n");
+    printf("$ test-mongocrypt test_mc_named_kms_provider_parse test_mc_named_kms_provider_map\n");
 
     /* Install all tests. */
     _mongocrypt_tester_install_crypto(&tester);
@@ -1067,7 +1070,17 @@ get_os_version_failed:
 
         if (argc > 1) {
             for (j = 1; j < argc; j++) {
-                found = (0 == strcmp(argv[j], tester.test_names[i]));
+                char *pattern = argv[j];
+                size_t pattern_len = strlen(pattern);
+                // Check for exact match:
+                found = (0 == strcmp(pattern, tester.test_names[i]));
+                // Check for trailing asterisk:
+                if (pattern[strlen(pattern) - 1] == '*') {
+                    if (0 == strncmp(pattern, tester.test_names[i], pattern_len - 1)) {
+                        found = true;
+                        break;
+                    }
+                }
                 if (found) {
                     break;
                 }
