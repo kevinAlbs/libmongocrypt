eefc6e511d2d6381d8d44d88aef410d717f0cbe4
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-15 09:06:21 -0500

    implement `mc_schema_broker_satisfy_from_encryptedFieldsMap`

--- src/mc-schema-broker-private.h
+++ src/mc-schema-broker-private.h
@@ -322,8 +322,45 @@ static inline bool mc_schema_broker_satisfy_from_encryptedFieldsMap(mc_schema_br
                                                                     mongocrypt_status_t *status) {
     BSON_ASSERT_PARAM(sb);
     BSON_ASSERT_PARAM(ef_map);
-    CLIENT_ERR("mc_schema_broker_satisfy_from_encryptedFieldsMap is not yet implemented");
-    return false;
+
+    for (mc_schema_entry_t *it = sb->ll; it != NULL; it = it->next) {
+        if (it->satisfied) {
+            continue;
+        }
+
+        bool loop_ok = false;
+        char *ns = bson_strdup_printf("%s.%s", sb->db, it->coll);
+        bson_iter_t iter;
+
+        if (bson_iter_init_find(&iter, ef_map, ns)) {
+            if (!_mongocrypt_buffer_copy_from_document_iter(&it->encryptedFields_buf, &iter)) {
+                CLIENT_ERR("failed to read encryptedFields from encryptedFields map for collection: %s", ns);
+                goto loop_fail;
+            }
+
+            bson_t ef_bson;
+            if (!_mongocrypt_buffer_to_bson(&it->encryptedFields_buf, &ef_bson)) {
+                CLIENT_ERR("failed to create BSON from encryptedFields for collection: %s", ns);
+                goto loop_fail;
+            }
+
+            if (!mc_EncryptedFieldConfig_parse(&it->encryptedFields, &ef_bson, status, true /* range v2 */)) {
+                goto loop_fail;
+            }
+
+            it->satisfied = true;
+            it->used_local_schema = true;
+        }
+
+        loop_ok = true;
+    loop_fail:
+        bson_free(ns);
+        if (!loop_ok) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 static inline bool mc_schema_broker_satisfy_from_cache(mc_schema_broker_t *sb,
--- test/test-mc-schema-broker.c
+++ test/test-mc-schema-broker.c
@@ -265,9 +265,58 @@ static void test_mc_schema_broker_satisfy_from_schemaMap(_mongocrypt_tester_t *t
     }
 }
 
+static void test_mc_schema_broker_satisfy_from_encryptedFieldsMap(_mongocrypt_tester_t *tester) {
+    bson_t *encryptedFieldsMap = TEST_FILE_AS_BSON("./test/data/schema-broker/encryptedFieldsMap.json");
+
+    // Can satisfy.
+    {
+        mongocrypt_status_t *status = mongocrypt_status_new();
+        mc_schema_broker_t *sb = mc_schema_broker_new();
+
+        ASSERT_OK_STATUS(mc_schema_broker_request(sb, "db", "coll", status), status);
+        ASSERT(mc_scheme_broker_need_more_schemas(sb));
+        ASSERT_OK_STATUS(mc_schema_broker_satisfy_from_encryptedFieldsMap(sb, encryptedFieldsMap, status), status);
+        ASSERT(!mc_scheme_broker_need_more_schemas(sb));
+
+        mc_schema_broker_destroy(sb);
+        mongocrypt_status_destroy(status);
+    }
+
+    // Does not satisfy with non-matching entry.
+    {
+        mongocrypt_status_t *status = mongocrypt_status_new();
+        mc_schema_broker_t *sb = mc_schema_broker_new();
+
+        ASSERT_OK_STATUS(mc_schema_broker_request(sb, "db", "coll", status), status);
+        ASSERT(mc_scheme_broker_need_more_schemas(sb));
+        ASSERT_OK_STATUS(mc_schema_broker_satisfy_from_encryptedFieldsMap(sb, TMP_BSON("{'db.foo': {}}"), status),
+                         status);
+        ASSERT(mc_scheme_broker_need_more_schemas(sb)); // Still not satisfied.
+
+        mc_schema_broker_destroy(sb);
+        mongocrypt_status_destroy(status);
+    }
+
+    // Fails to satisfy with empty entry.
+    {
+        mongocrypt_status_t *status = mongocrypt_status_new();
+        mc_schema_broker_t *sb = mc_schema_broker_new();
+
+        ASSERT_OK_STATUS(mc_schema_broker_request(sb, "db", "coll", status), status);
+        ASSERT(mc_scheme_broker_need_more_schemas(sb));
+        ASSERT_FAILS_STATUS(mc_schema_broker_satisfy_from_encryptedFieldsMap(sb, TMP_BSON("{'db.coll': {}}"), status),
+                            status,
+                            "unable to find 'fields'");
+
+        mc_schema_broker_destroy(sb);
+        mongocrypt_status_destroy(status);
+    }
+}
+
 void _mongocrypt_tester_install_mc_schema_broker(_mongocrypt_tester_t *tester) {
     INSTALL_TEST(test_mc_schema_broker_request);
     INSTALL_TEST(test_mc_schema_broker_satisfy_from_collInfo);
     INSTALL_TEST(test_mc_schema_broker_satisfy_from_cache);
     INSTALL_TEST(test_mc_schema_broker_satisfy_from_schemaMap);
+    INSTALL_TEST(test_mc_schema_broker_satisfy_from_encryptedFieldsMap);
 }
