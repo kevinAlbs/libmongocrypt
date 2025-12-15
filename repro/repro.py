7183739ded41c5383b74ca12b65b133229f68309
Kevin Albertson <kevin.albertson@mongodb.com>
2025-12-15 15:57:13 -0500

    add more instructions

--- repro/repro.py
+++ repro/repro.py
@@ -1,18 +1,28 @@
+"""
+To test, download:
+- https://downloads.mongodb.com/linux/mongo_crypt_shared_v1-linux-x86_64-enterprise-ubuntu2204-7.0.25.tgz
+- https://downloads.mongodb.com/linux/mongo_crypt_shared_v1-linux-x86_64-enterprise-ubuntu2404-8.0.15.tgz
+
+To test an explicit path:
+
+    CRYPT_SHARED_PATH=$HOME/crypt_shared/7.0.25/lib/mongo_crypt_v1.so python repro.py
+
+To test a system path:
+
+    rm -rf /usr/lib/mongo_crypt_v1.so
+    sudo ln -s $HOME/crypt_shared/7.0.25/lib/mongo_crypt_v1.so /usr/lib/mongo_crypt_v1.so
+    python repro.py
+"""
+
 import os
 from pymongo import MongoClient
 from pymongo.encryption_options import AutoEncryptionOpts
 
-if "CRYPT_SHARED_PATH" not in os.environ:
-    raise Exception("Set CRYPT_SHARED_PATH")
-
-kms_providers = {"local": {"key": os.urandom(96)}}
-
 auto_encryption_opts = AutoEncryptionOpts(
-    kms_providers,
-    "keyvault.datakeys",
-    crypt_shared_lib_path=os.environ["CRYPT_SHARED_PATH"],
+    kms_providers={"local": {"key": os.urandom(96)}},
+    key_vault_namespace="keyvault.datakeys",
+    crypt_shared_lib_required=True,
+    crypt_shared_lib_path=os.environ.get("CRYPT_SHARED_PATH"),
 )
 client = MongoClient(auto_encryption_opts=auto_encryption_opts)
-client.db.drop_collection("coll")
-coll = client.db.create_collection("coll")
-coll.insert_one({"foo": "bar"})
+client["db"]["coll"].insert_one({"foo": "bar"}) # Attempts auto encryption.
