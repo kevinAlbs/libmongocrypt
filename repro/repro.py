4a3bcf1c487df6502c34d4fe687114deef0f0f1b
Kevin Albertson <kevin.albertson@mongodb.com>
2025-12-17 11:12:27 -0500

    also create `ClientEncryption` in repro

--- repro/repro.py
+++ repro/repro.py
@@ -17,12 +17,22 @@ To test a system path:
 import os
 from pymongo import MongoClient
 from pymongo.encryption_options import AutoEncryptionOpts
+from pymongo.encryption import ClientEncryption
+
+kms_providers = {"local": {"key": os.urandom(96)}}
+key_vault_namespace = "keyvault.datakeys"
 
 auto_encryption_opts = AutoEncryptionOpts(
-    kms_providers={"local": {"key": os.urandom(96)}},
-    key_vault_namespace="keyvault.datakeys",
+    kms_providers=kms_providers,
+    key_vault_namespace=key_vault_namespace,
     crypt_shared_lib_required=True,
     crypt_shared_lib_path=os.environ.get("CRYPT_SHARED_PATH"),
 )
 client = MongoClient(auto_encryption_opts=auto_encryption_opts)
 client["db"]["coll"].insert_one({"foo": "bar"}) # Attempts auto encryption.
+
+client_encryption = ClientEncryption(
+    kms_providers=kms_providers,
+    key_vault_namespace=key_vault_namespace,
+    key_vault_client=client,
+    codec_options=client.codec_options)
